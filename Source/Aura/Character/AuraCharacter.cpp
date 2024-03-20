eebdeaa422cc12c49c332677ee59e7ce5552a1dc
Ivan Huliakevich <amaz1ng.hulia@gmail.com>
2024-03-20 20:40:27 +0300

    Base Character added

---
+++ Source/Aura/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "AuraCharacter.h"
+
---
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -0,0 +1,12 @@
+#include "AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
